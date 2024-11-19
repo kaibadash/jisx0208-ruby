bc5a1667044293123dc560eef45a94dbcdd82072
kaiba <kaibadash@gmail.com>
2024-11-19 11:52:37 +0900

    :bug: Fix invalid jisx range checking

--- spec/jisx0208_spec.rb
+++ spec/jisx0208_spec.rb
@@ -6,17 +6,23 @@ RSpec.describe JISX0208::Code do
   let(:code) { JISX0208::Code.new }
 
   it "contains first level kanji" do
-    expect(code.contains_first_level_kanji?("亜")).to eq(true)
-    expect(code.contains_first_level_kanji?("弌")).to eq(false)
-    expect(code.contains_first_level_kanji?("亜X")).to eq(true)
-    expect(code.contains_first_level_kanji?("弌X")).to eq(false)
+    %w[亜 腕 駐 車 場 回 転 寿 司].each do |char|
+      expect(code.contains_first_level_kanji?(char)).to eq(true)
+    end
+
+    %w[偃 龠 假 會 偕 偐 偈 做 偖].each do |char|
+      expect(code.contains_first_level_kanji?(char)).to eq(false)
+    end
   end
 
   it "contains second level kanji" do
-    expect(code.contains_second_level_kanji?("亜")).to eq(false)
-    expect(code.contains_second_level_kanji?("弌")).to eq(true)
-    expect(code.contains_second_level_kanji?("亜X")).to eq(false)
-    expect(code.contains_second_level_kanji?("弌X")).to eq(true)
+    %w[亜 腕 駐 車 場 回 転 寿 司].each do |char|
+      expect(code.contains_second_level_kanji?(char)).to eq(false)
+    end
+
+    %w[偃 龠 假 會 偕 偐 偈 做 偖].each do |char|
+      expect(code.contains_second_level_kanji?(char)).to eq(true)
+    end
   end
 
   it "contains jisx0208 kanji" do
@@ -24,5 +30,6 @@ RSpec.describe JISX0208::Code do
     expect(code.contains_jisx0208_kanji?("弌")).to eq(true)
     expect(code.contains_jisx0208_kanji?("亜X")).to eq(true)
     expect(code.contains_jisx0208_kanji?("弌X")).to eq(true)
+    expect(code.contains_jisx0208_kanji?("JIS")).to eq(false)
   end
 end
